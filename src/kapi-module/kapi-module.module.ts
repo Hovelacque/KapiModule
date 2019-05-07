8f0c1f4436700adf8f2c806c4e3a6dac6f287c2d
Gustavo <gustavoinfomax@yahoo.com.br>
2019-05-07 12:17:32 -0300

    Criada tela para teste

---
+++ src/kapi-module/kapi-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TelaTesteComponent } from './tela-teste/tela-teste.component';
+
+@NgModule({
+  declarations: [
+    TelaTesteComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  providers: [],
+})
+export class KapiModuleModule { }
---
+++ src/kapi-module/tela-teste/tela-teste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tela-teste',
+  templateUrl: './tela-teste.component.html',
+  styleUrls: ['./tela-teste.component.css']
+})
+export class TelaTesteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
